7cd1185c8c38554f59808d97fa9fce0df8c7ff59
YaoveiD <72921399+YaoveiD@users.noreply.github.com>
2022-04-18 10:27:11 +0800

    Delete Library directory

--- Library/various/ckmin.cpp
+++
@@ -1,3 +0,0 @@
-
-template<class T> inline bool ckmin(T& a, const T& b) { return b < a ? a = b, true : false; }
-template<class T> inline bool ckmax(T& a, const T& b) { return a < b ? a = b, true : false; }
